35684ff3801de5c74688886029d728f3bf27f858
Alejandro Astu <jastudiagu@educacion.navarra.es>
2023-10-05 16:36:53 +0200

    CORRECION EJERCICIO 9

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -2,9 +2,7 @@ cantidad = int(input('Indica la cantidad a invertir: '))
 ia = float(input('Indica el interés anual (en %): '))
 na = int(input('Indica el número de años: '))
 
+for i in range(na):
+    cantidad = cantidad + cantidad * (ia/100)
 
-operacion = (cantidad * ia / 100) * na
-resultado = cantidad + operacion
-
-
-print('El capital que obtendras será: ', resultado, '€')
+print('El capital que obtendras será: ', cantidad, '€')
